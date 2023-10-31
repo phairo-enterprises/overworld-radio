b2ac16fe4e831c13efe8d399f048f107ce21122a
phairo-enterprises <149274898+phairo-enterprises@users.noreply.github.com>
2023-10-31 14:53:12 -0700

    Update loadMDfile.js

--- js/loadMDfile.js
+++ js/loadMDfile.js
@@ -1,11 +1,14 @@
 // This function fetches, converts, and renders a specified Markdown file.
 function loadMDfile(filename) {
+    console.log('loadMDfile function called with filename:', filename);
+
     // Inform the user that the loading process has started.
     logToConsole(`Attempting to load ${filename}...`);
 
     // Fetch the content of the provided file.
     fetch(filename)
         .then(response => {
+            console.log('Fetch response received:', response);
             // Handle non-successful HTTP responses.
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
@@ -13,6 +16,7 @@ function loadMDfile(filename) {
             return response.text();
         })
         .then(content => {
+            console.log('Fetched content:', content.substring(0, 100) + "...");  // Log the first 100 chars for brevity
             // Convert the fetched Markdown content into HTML using the 'marked' library.
             const renderedHTML = marked(content);
 
@@ -23,6 +27,7 @@ function loadMDfile(filename) {
             logToConsole(`${filename} loaded and rendered successfully.`);
         })
         .catch(error => {
+            console.error('Error in loadMDfile:', error);
             // In case of any errors, inform the user with the specific error message.
             logToConsole(`Failed to load ${filename}. Error: ${error.message}`, "error");
         });
