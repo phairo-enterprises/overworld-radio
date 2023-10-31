f9c279052a80d6f86e353eaf0da536d6f5564777
phairo-enterprises <149274898+phairo-enterprises@users.noreply.github.com>
2023-10-31 11:37:04 -0700

    end ckpt 1

--- js/loadMDfile.js
+++ js/loadMDfile.js
@@ -1,29 +1,29 @@
-// Function responsible for fetching, converting, and rendering a specified Markdown file.
+// This function fetches, converts, and renders a specified Markdown file.
 function loadMDfile(filename) {
-    // Log the action for user awareness.
+    // Inform the user that the loading process has started.
     logToConsole(`Attempting to load ${filename}...`);
 
-    // Fetch the content of the specified file.
+    // Fetch the content of the provided file.
     fetch(filename)
         .then(response => {
-            // If the fetch wasn't successful, throw an error to be caught later.
+            // Handle non-successful HTTP responses.
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
             return response.text();
         })
         .then(content => {
-            // Use the 'marked' library to convert the fetched Markdown content into HTML.
+            // Convert the fetched Markdown content into HTML using the 'marked' library.
             const renderedHTML = marked(content);
 
-            // Inject the converted content into the designated area on the webpage.
+            // Display the converted HTML content in the designated content area.
             document.getElementById('contentArea').innerHTML = renderedHTML;
 
-            // Inform the user of the successful rendering.
+            // Update the user about the successful rendering of the file.
             logToConsole(`${filename} loaded and rendered successfully.`);
         })
         .catch(error => {
-            // Log and inform the user of any encountered errors.
+            // In case of any errors, inform the user with the specific error message.
             logToConsole(`Failed to load ${filename}. Error: ${error.message}`, "error");
         });
 }
